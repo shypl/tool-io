2893fbbf0cdf092a305534b57a51fbed5e29f213
Alexey Chuprin <a.chuprin@capjack.ru>
2026-01-22 17:33:11 +0300

    kotlin 2.3.0 gradle 9.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	kotlin("jvm") version "2.1.10"
+	kotlin("jvm") version "2.3.0"
 	id("java-library")
 	id("maven-publish")
 	id("nebula.release") version "19.0.10"
@@ -20,7 +20,7 @@ repositories {
 }
 
 dependencies {
-	implementation("org.shypl.tool:tool-lang:1.0.0")
+	implementation("org.shypl.tool:tool-lang:1.0.1")
 	testImplementation(kotlin("test"))
 }
 
